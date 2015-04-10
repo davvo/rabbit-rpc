dcab443d8fe52175ff94eb86a7dbdffb9ee25d36
David <david.ershag@gmail.com>
2015-04-10 18:52:36 +0200

    Added example

---
+++ example/square.js
@@ -0,0 +1,15 @@
+var rpc = require('..')();
+
+rpc.handle('square', function (num, callback) {
+    setTimeout(function () {
+        callback(Math.pow(num, 2));
+    }, Math.random() * 1000);
+});
+
+function printSqure(num) {
+    rpc.call('square', num, function (res) {
+        console.log('The square of %d is', num, res);
+    });
+}
+
+[1, 2, 3, 4, 5].forEach(printSqure);
