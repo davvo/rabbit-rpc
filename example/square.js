7291418183bda4a8b360a5560a40b117c566052f
David Ershag <david.ershag@gmail.com>
2015-04-11 16:17:13 +0200

    Update square.js

--- example/square.js
+++ example/square.js
@@ -2,12 +2,12 @@ var rpc = require('..')();
 
 rpc.handle('square', function (num, callback) {
     setTimeout(function () {
-        callback(Math.pow(num, 2));
+        callback(null, Math.pow(num, 2));
     }, Math.random() * 1000);
 });
 
 function printSquare(num) {
-    rpc.call('square', num, function (res) {
+    rpc.call('square', num, function (err, res) {
         console.log('The square of %d is', num, res);
     });
 }
